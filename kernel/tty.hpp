bbe4110bcdd95ef8f5f37419ed1a6320ecabd362
Jacob Ellis Duncan <jellisduncan@gmail.com>
2021-07-27 05:47:18 -0500

    fix a few warnings

--- kernel/tty.hpp
+++ kernel/tty.hpp
@@ -8,12 +8,12 @@
 
 using namespace vga;
 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
+static size_t terminal_row;
+static size_t terminal_column;
+static uint8_t terminal_color;
+static uint16_t* terminal_buffer;
 
-size_t strlen(const char* str) 
+static size_t strlen(const char* str) 
 {
 	size_t len = 0;
 	while (str[len])
